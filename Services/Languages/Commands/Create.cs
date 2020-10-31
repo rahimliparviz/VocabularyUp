96bb76e1d19f67616448dc599ee464e51924b9bb
rahimliparviz <rahimliparviz@gmail.com>
2020-11-01 02:31:55 +0400

    Rest error handler added.

---
+++ Services/Languages/Commands/Create.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.Languages.Commands
+{
+    public class Create
+    {
+        public class Command:IRequest
+        {
+            public string Name { get; set; }
+        }
+        
+        public class Handler:IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var language =await _context.Languages.AnyAsync(l=>l.Name == request.Name);
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Services/Languages/Queries/Details.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Data;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.Languages.Queries
+{
+    public class Details
+    {
+        public class Query : IRequest<Language> {
+            public Guid Id { get; set; }
+        }
+        
+        
+        public class Handler:IRequestHandler<Query,Language>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Language> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var language = await _context.Languages.FirstOrDefaultAsync(l => l.Id == request.Id);
+
+                _ = language ?? throw new Exception();
+                
+                return language;
+            }
+        }
+    }
+}
