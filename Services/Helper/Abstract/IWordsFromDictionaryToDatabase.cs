a7f2931454ab38a4598e4b3a3deb23c51c031b03
rahimliparviz <rahimliparviz@gmail.com>
2020-12-19 01:53:54 +0400

    Basic "Dictionary" adding functionality added need to improvement.And Swagger added

---
+++ Services/Helper/Abstract/IWordsFromDictionaryToDatabase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Services.Helper.Abstract
+{
+    public interface IWordsFromDictionaryToDatabase
+    {
+        void AddWordsFile(IFormFile file);
+    }
+}
