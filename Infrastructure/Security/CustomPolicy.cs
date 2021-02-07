895179b4cf58de38a1a05ef9ed6c2885615f459c
rahimliparviz <rahimliparviz@gmail.com>
2021-02-07 23:12:38 +0400

    Small changes due to "frontend" authentication functionality.

---
+++ Infrastructure/Security/CustomPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Infrastructure.Security
+{
+    public class CustomPolicy: IAuthorizationRequirement
+    {
+        
+    }
+
+    public class CustomPolicyHandler : AuthorizationHandler<CustomPolicy>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomPolicy requirement)
+        {
+            // context.Succeed(requirement);
+            context.Fail();
+            return Task.FromResult(0);
+        }
+    }
+}
